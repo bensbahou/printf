fc1df21340d210d23bf17c00bc670712a70c5eee
salahelec2 <salahelec2@gmail.com>
2023-04-26 15:12:26 -0700

    main.h repaired

--- main.h
+++ main.h
@@ -20,11 +20,12 @@ typedef struct print
 	int (*f)(va_list);
 } print_sym_func;
 
+print_sym_func *init_print_sym(void);
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_string(va_list);
 int print_integer(va_list);
 int _print_aux(const char *format, va_list args, print_sym_func *print_sym);
-print_sym_func *init_print_sym(void);
 
 #endif /* MAIN_H */
